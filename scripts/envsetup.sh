b9a13ce01be438d6a0f235e6a40e0b5ae225f474
DemonDrake <31922309+DemonDrake@users.noreply.github.com>
2023-03-19 00:08:06 +0800

    envsetup: cleanup

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -1,9 +1,1 @@
-#!/bin/sh
-
-set -e -x
-
-CLANG_URL="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android13-release/clang-r450784d.tar.gz"
-
 sudo apt-get update && sudo apt-get install bc git zip make tar libncurses* gcc clang python3 openssl util-linux perl binutils e2fsprogs udev build-essential bison flex libssl-dev libelf-dev -y
-
-if [ ! -d ${CLANG_DIR} ]; then mkdir ${CLANG_DIR} && curl "${CLANG_URL}" -o clang.tgz && tar -xzf clang.tgz -C ${CLANG_DIR}; fi
