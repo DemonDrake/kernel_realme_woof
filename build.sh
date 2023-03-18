7ee2c5e654afb4ee247fe6e31ff842081141eae8
DemonDrake <31922309+DemonDrake@users.noreply.github.com>
2023-03-18 22:39:53 +0800

    build: remove prebuilt tc
    - change cross compiler

--- build.sh
+++ build.sh
@@ -2,36 +2,33 @@
 
 set -e -x
 
-# Final Zip Name
-ZIPNAME="Woof-Kernel-test.zip"
-
 # Kernel Config
 KERNEL_DEFCONFIG="vendor/lahaina-qgki_defconfig"
+
+# Clang directory
+CLANG_DIR="clang"
 
 # Prebuilt Clang Toolchain (AOSP)
 CLANG_URL="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android13-release/clang-r450784d.tar.gz"
 
-# Prebuilt GCC Utilities (AOSP)
-GCC_x64="https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9"
-GCC_x32="https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9"
-GCC_BRANCH="android12L-release"
+# Final Zip Name
+ZIPNAME="Woof-RMX3461-$(date '+%Y%m%d-%H%M').zip"
 
 # Setup make Command
 make_fun() {
 	make O=out ARCH=arm64 \
 		CC=clang HOSTCC=clang \
+		CROSS_COMPILE=${CLANG_DIR}/bin/llvm- \
+		LLVM=1 \
+		LLVM_IAS=1 \
 		CLANG_TRIPLE=aarch64-linux-gnu- \
-		CROSS_COMPILE=aarch64-linux-android- \
-		CROSS_COMPILE_ARM32=arm-linux-androideabi- "$@" 
 }
 
 # Cloning all the Necessary files
-if [ ! -d clang ]; then mkdir clang && curl "${CLANG_URL}" -o clang.tgz && tar -xzf clang.tgz -C clang; fi
-[ ! -d x64 ] && git clone --depth=1 "${GCC_x64}" -b "${GCC_BRANCH}" x64
-[ ! -d x32 ] && git clone --depth=1 "${GCC_x32}" -b "${GCC_BRANCH}" x32
+if [ ! -d ${CLANG_DIR} ]; then mkdir ${CLANG_DIR} && curl "${CLANG_URL}" -o clang.tgz && tar -xzf clang.tgz -C ${CLANG_DIR}; fi
 
 # Setting Toolchain Path
-PATH="${PWD}/clang/bin:${PWD}/x64/bin:${PWD}/x32/bin:/bin"
+PATH="${CLANG_DIR}/bin:${PATH}"
 
 # Installing KernelSU
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
